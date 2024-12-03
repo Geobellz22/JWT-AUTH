18ca12a10603335af2023cbbc2eb0a7a1931f676
Geobellz22 <106580682+Geobellz22@users.noreply.github.com>
2024-12-03 14:37:49 +0100

    Update wsgi.py

--- jwt_auth/wsgi.py
+++ jwt_auth/wsgi.py
@@ -1,16 +1,26 @@
 """
-WSGI config for jwt_auth project.
+ASGI config for jwt_auth project.
 
-It exposes the WSGI callable as a module-level variable named ``application``.
+It exposes the ASGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
+https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
 """
 
 import os
-
-from django.core.wsgi import get_wsgi_application
+from django.core.asgi import get_asgi_application
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
+import chat.routing
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'jwt_auth.settings')
 
-application = get_wsgi_application()
+# Define the ASGI application
+application = ProtocolTypeRouter({
+    "http": get_asgi_application(),
+    "websocket": AuthMiddlewareStack(
+        URLRouter(
+            chat.routing.websocket_urlpatterns
+        )
+    ),
+})
